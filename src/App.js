ce6706e5cb09c5201063a616dc3d067b0b4b7371
Plácido Mota <38774223+placidomota@users.noreply.github.com>
2021-11-24 22:57:51 -0500

    Delete src directory

--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-   <h1>Bem vindos ao React</h1>
-  );
-}
-
-export default App;
--- src/Header.js
+++
@@ -1,5 +0,0 @@
-function Header() {
-    return <header class="header"><h1>Meu Primeiro site em React</h1></header>;
-}
-
-export default Header;
